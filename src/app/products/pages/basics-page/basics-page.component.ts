8a5d50c366c1b1466711da461dfaacb2128acd0d
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-04-29 20:00:01 +0200

    Módulos y Rutas de la App

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basics-page.component.html',
+  styleUrls: ['./basics-page.component.css']
+})
+export class BasicsPageComponent {
+
+}
