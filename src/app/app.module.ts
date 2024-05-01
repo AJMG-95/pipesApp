e08bed9b05befe7684923e42d569152956eaf246
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-05-01 11:55:12 +0200

    cambia el idioma global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -7,7 +7,14 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
+//Configuacion del locale de la app
+import localeEs from "@angular/common/locales/es-419"
+import localeFrCA from "@angular/common/locales/fr-CA"
 
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs)
+registerLocaleData(localeFrCA)
 
 @NgModule({
   declarations: [
@@ -19,7 +26,11 @@ import { AppComponent } from './app.component';
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-419'  //Definir por defecto un idioma (es-419) en toda la app
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
